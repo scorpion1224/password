0b669dcb96a06a8a6fe273ab26314af6d59ed860
Eddie Zhang <zhangzian1996@gmail.com>
2018-08-14 10:34:28 +0800

    v1

--- pw.py
+++ pw.py
@@ -1,14 +1,19 @@
 password = 'a123456'
 i = 3 #剩余机会
-while True:
+while i > 0:
+	i = i - 1
 	pwd = input('请输入你的密码：')
 	if pwd == password:
 		print('登陆成功!')
 		break
 	else:
-		i = i - 1
-		print('密码错误！还有' , i ,'次机会')
-		if i == 0:
-			break
+		
+		print('密码错误！')
+		if i > 0:
+			print('密码错误！还有' , i ,'次机会')
+		else:
+			print('没机会了')
+
+		
 
 
